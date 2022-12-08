6a7857cc0875478a2f1824575a3b98929e09cad3
MinayoMelanie <melanieminayo00@gmail.com>
2022-12-08 09:24:02 -0800

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *description:
+ *puts() - used to write a line or string to the output (stdout)stream
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
